27f7a6d2be3c0f01edef81363c4eff736a892974
Ambika Mishra <102850517+ambikamishra03@users.noreply.github.com>
2025-04-16 01:24:14 +0530

    build

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './components/Home.js'
-import QuizePage from "./components/QuizePage.js";
+import Home from './components/Home'
+import QuizePage from './components/QuizePage';
 
 
 const  App = ()  => {
